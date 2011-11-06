2088806eaf265c20208c37872ed552f80e96803f
Dorvaryn <benjamin@odai.homelinux.net>
2011-11-06 11:12:24 +0100

    Ajout de comportements usuels à l'IA + vérification des coups non autorisés sur le numéro de colone aussi.x

--- README.md
+++ README.md
@@ -7,9 +7,8 @@ Un petit jeu de Puissance 4 en Prolog
 * Ghislaine El Azhar <gelazhar@gmail.com>
 
 ## Etat ##
-Le code est encore en chantier
+Projet terminé.
+Une IA utilisant min max pourra être implémentée par la suite sur ce projet histoire de voir l'apport sur ce type de jeu d'un min max face à une IA gloutonne + quelques comportements usuels.
 
 ### TODO ###
 * Implémentation minmax
-* Controle d'une entrée de jeu de plus de 7 colones(éviter le false)
-* Vérifier finJeu ou l'IA afin de trouver pourquoi dans certains cas l'IA ne joue pas le coup gagnant(peut être que finJeu ne détecte pas tout les cas de victoire)
--- puissance4.pl
+++ puissance4.pl
@@ -1,11 +1,14 @@
 max(X,Y,Y) :- Y>X, !.
 max(X,Y,X). 
 
+% Fonction qui permet d'ajouter un élément en fin de liste
 ajoutFin(X,[],[X]).
 ajoutFin(X,[Y|L1],[Y|L2]):- ajoutFin(X,L1,L2).
 
+finListe([], _).
+finListe(L, E):- last(L,E).
+
 % Fonction qui renvoie une sous-liste à partir d'une liste L
-% description...
 /* Paramètres : S sous-liste, L liste */
 prefix(P,L):-append(P,_,L).
 sublist(S,L):-prefix(S,L).
@@ -15,7 +18,7 @@ sublist(S,[_|T]):-sublist(S,T).
 /* Paramètres : L liste, N longueur de la liste */
 longueur([],0).
 longueur([_|L],N):- longueur(L,N1),
-					 N is N1+1.
+					N is N1+1.
 
 % Fonction qui renvoie le nième élément d'une liste 
 /* Paramètres : N index de l'élement qu'on veut récupérer, L liste, X élément retourné */
@@ -24,15 +27,18 @@ nthElem(N, L, X):- nth1(N, L, X).
 
 % Fonction qui enregistre un coup joué dans la grille
 /* Paramètres : N numéro de la colonne dans laquelle J joue, G grille, J joueur, G' nouvelle grille */		
-enregistrerCoup(1, [L|G], a, _, I):- longueur(L,N), N >= 6, write('Coup Invalide\n'), jouerCoupA(I).
-enregistrerCoup(1, [L|G], b, _, I):- longueur(L,N), N >= 6, write('Coup Invalide\n'), jouerCoupB(I).
+enregistrerCoup(1, [L|G], x, _, I):- longueur(L,N), N >= 6, write('Coup Invalide\n'), jouerCoupX(I).
+enregistrerCoup(1, [L|G], o, _, I):- longueur(L,N), N >= 6, write('Coup Invalide\n'), jouerCoupO(I).
 enregistrerCoup(1, [L|G], J, F, I):- longueur(L,N), N < 6, ajoutFin(J,L,M), F=[M|G].
+enregistrerCoup(N, [L|G], x, _, I):- N > 7, write('Coup Invalide\n'), jouerCoupX(I).
+enregistrerCoup(N, [L|G], o, _, I):- N > 7, write('Coup Invalide\n'), jouerCoupO(I).
 enregistrerCoup(N, [T|X], J, [T|G], I):- 	N > 0,
 										N1 is N-1,
 										enregistrerCoup(N1, X, J, G, I).
-
-enregistrerCoupJoueur(1, [L|G], a, _, I):- longueur(L,N), N >= 6, write('Coup Invalide\n'), jouerCoupJoueur(I).
+					
+enregistrerCoupJoueur(1, [L|G], x, _, I):- longueur(L,N), N >= 6, write('Coup Invalide\n'), jouerCoupJoueur(I).
 enregistrerCoupJoueur(1, [L|G], J, F, I):- longueur(L,N), N < 6, ajoutFin(J,L,M), F=[M|G].
+enregistrerCoupJoueur(N, [L|G], x, _, I):- N > 7, write('Coup Invalide\n'), jouerCoupJoueur(I).	
 enregistrerCoupJoueur(N, [T|X], J, [T|G], I):- 	N > 0,
 										N1 is N-1,
 										enregistrerCoupJoueur(N1, X, J, G, I).
@@ -87,12 +93,12 @@ finJeuDiag(G,J):- finJeuDiag(G,1,[],J).
 
 % Définition et test des conditions de fin de jeu
 /* Paramètres : G grille, J joueur */
-finJeu(G, J):- finJeuVert(G,a), J=a.
-finJeu(G, J):- finJeuVert(G,b), J=b.
-finJeu(G, J):- finJeuHor(G,a), J=a.
-finJeu(G, J):- finJeuHor(G,b), J=b.
-finJeu(G, J):- finJeuDiag(G,a), J=a.
-finJeu(G, J):- finJeuDiag(G,b), J=b.
+finJeu(G, J):- finJeuVert(G,x), J=x.
+finJeu(G, J):- finJeuVert(G,o), J=o.
+finJeu(G, J):- finJeuHor(G,x), J=x.
+finJeu(G, J):- finJeuHor(G,o), J=o.
+finJeu(G, J):- finJeuDiag(G,x), J=x.
+finJeu(G, J):- finJeuDiag(G,o), J=o.
 
 % Affichage du gagnant
 /* Paramètres : J joueur */
@@ -100,21 +106,21 @@ gagnant(J):-write('Le Joueur '), write(J), write(' a gagné !').
 
 
 /* Paramètres : G grille*/
-jouerCoupA(G):-finJeu(G,J), gagnant(J),!.
-jouerCoupB(G):-finJeu(G,J), gagnant(J),!.
-jouerCoupA(G):- write('Joueur A, entrez un numéro de colonne : '),
-				read(N), enregistrerCoup(N,G, a, X, G),
+jouerCoupX(G):-finJeu(G,J), gagnant(J),!.
+jouerCoupO(G):-finJeu(G,J), gagnant(J),!.
+jouerCoupX(G):- write('Joueur x, entrez un numéro de colonne : '),
+				read(N), enregistrerCoup(N,G, x, X, G),
 				afficherGrille(X),
 				write('\n'),
-				jouerCoupB(X).
-jouerCoupB(G):- write('Joueur B, entrez un numéro de colonne : '),
-				read(N), enregistrerCoup(N,G, b, X, G),
+				jouerCoupO(X).
+jouerCoupO(G):- write('Joueur o, entrez un numéro de colonne : '),
+				read(N), enregistrerCoup(N,G, o, X, G),
 				afficherGrille(X),
 				write('\n'),
-				jouerCoupA(X).
+				jouerCoupX(X).
 
-% Lancement du jeu : grille de départ de 6*7 (vide). C'est le joueur 'a' qui commence, suivi par b, jusqu'à ce que l'un des deux gagne [ou GRILLE PLEINE]
-jouer:- jouerCoupA([[],[],[],[],[],[],[]]).
+% Lancement du jeu : grille de départ de 6*7 (vide). C'est le joueur 'o' qui commence, suivi par x, jusqu'à ce que l'un des deux gagne [ou GRILLE PLEINE]
+jouer:- jouerCoupO([[],[],[],[],[],[],[]]).
 
 %Un coup gagant est un coup qui mene à un état de jeu ou le joueur est vainqueur
 coupGagnant(C,G,J):- enregistrerCoupIA(1,G,J,N,G), finJeu(N,J), C=1.
@@ -125,17 +131,26 @@ coupGagnant(C,G,J):- enregistrerCoupIA(5,G,J,N,G), finJeu(N,J), C=5.
 coupGagnant(C,G,J):- enregistrerCoupIA(6,G,J,N,G), finJeu(N,J), C=6.
 coupGagnant(C,G,J):- enregistrerCoupIA(7,G,J,N,G), finJeu(N,J), C=7.
 
+%Un coup perdant est un coup qui permet à l'adversaire de gagner
+coupPerdantIA(1,G):- enregistrerCoupIA(1,G,o,N,G), coupGagnant(D,N,x).
+coupPerdantIA(2,G):- enregistrerCoupIA(2,G,o,N,G), coupGagnant(D,N,x).
+coupPerdantIA(3,G):- enregistrerCoupIA(3,G,o,N,G), coupGagnant(D,N,x).
+coupPerdantIA(4,G):- enregistrerCoupIA(4,G,o,N,G), coupGagnant(D,N,x).
+coupPerdantIA(5,G):- enregistrerCoupIA(5,G,o,N,G), coupGagnant(D,N,x).
+coupPerdantIA(6,G):- enregistrerCoupIA(6,G,o,N,G), coupGagnant(D,N,x).
+coupPerdantIA(7,G):- enregistrerCoupIA(7,G,o,N,G), coupGagnant(D,N,x).
+
 jouerCoupJoueur(G):-finJeu(G,J), gagnant(J),!.
 jouerIA(G):-finJeu(G,J), gagnant(J),!.
 
 %Si un coup permet de gagner il faut le jouer.
-jouerIA(G):-   coupGagnant(C,G,b), enregistrerCoupIA(C,G,b,X,G),
+jouerIA(G):-   coupGagnant(C,G,o), enregistrerCoupIA(C,G,o,X,G),
 			   afficherGrille(X),
 			   write('\n'),
 			   jouerCoupJoueur(X).
 
-%Si un coup permet a l'adversaire de jouer on se défend.
-jouerIA(G):-   coupGagnant(C,G,a), enregistrerCoupIA(C,G,b,X,G),
+%Si un coup permet a l'adversaire de gagner on se défend(coup défensif).
+jouerIA(G):-   coupGagnant(C,G,x), enregistrerCoupIA(C,G,o,X,G),
 			   afficherGrille(X),
 			   write('\n'),
 			   jouerCoupJoueur(X).
@@ -143,12 +158,35 @@ jouerIA(G):-   coupGagnant(C,G,a), enregistrerCoupIA(C,G,b,X,G),
 jouerIA(0, G):- write('Pas de coup trouvé').
 
 
-jouerIA(C, G):- enregistrerCoupIA(C,G,b,X,G),
+jouerIA(C, G):- enregistrerCoupIA(C,G,o,X,G),
 			    afficherGrille(X),
 			    write('\n'),
 			    jouerCoupJoueur(X).
 
-%Si on a pas de coup immédiat on fait un coup au centre ou au plus près possible.
+espaceRestant(1, [L|G], E, L):- longueur(L,N2), N3 is 6-N2, E=N3.
+espaceRestant(N, [T|X], E, L):- N > 0,
+								N1 is N-1,
+								espaceRestant(N1, X, E, L).
+									
+%Si on a pas de coup immédiat on fait un coup au centre ou au plus près possible pour une victoire possible en verticale.
+jouerIA(G):- espaceRestant(4,G,E,L), finListe(L,o), E > 3, not(coupPerdantIA(4,G)), jouerIA(4,G).
+jouerIA(G):- espaceRestant(5,G,E,L), finListe(L,o), E > 3, not(coupPerdantIA(5,G)), jouerIA(5,G).
+jouerIA(G):- espaceRestant(3,G,E,L), finListe(L,o), E > 3, not(coupPerdantIA(3,G)), jouerIA(3,G).
+jouerIA(G):- espaceRestant(6,G,E,L), finListe(L,o), E > 3, not(coupPerdantIA(6,G)), jouerIA(6,G).
+jouerIA(G):- espaceRestant(2,G,E,L), finListe(L,o), E > 3, not(coupPerdantIA(2,G)), jouerIA(2,G).
+jouerIA(G):- espaceRestant(7,G,E,L), finListe(L,o), E > 3, not(coupPerdantIA(7,G)), jouerIA(7,G).
+jouerIA(G):- espaceRestant(1,G,E,L), finListe(L,o), E > 3, not(coupPerdantIA(1,G)), jouerIA(1,G).
+
+%Sinon jouer au plus près du centre quand même.
+jouerIA(G):- jouerIA(4,G),not(coupPerdantIA(4,G)).
+jouerIA(G):- jouerIA(5,G),not(coupPerdantIA(5,G)).
+jouerIA(G):- jouerIA(3,G),not(coupPerdantIA(3,G)).
+jouerIA(G):- jouerIA(6,G),not(coupPerdantIA(6,G)).
+jouerIA(G):- jouerIA(2,G),not(coupPerdantIA(2,G)).
+jouerIA(G):- jouerIA(7,G),not(coupPerdantIA(7,G)).
+jouerIA(G):- jouerIA(1,G),not(coupPerdantIA(1,G)).
+
+%Déblocage de situation
 jouerIA(G):- jouerIA(4,G).
 jouerIA(G):- jouerIA(5,G).
 jouerIA(G):- jouerIA(3,G).
@@ -158,8 +196,8 @@ jouerIA(G):- jouerIA(7,G).
 jouerIA(G):- jouerIA(1,G).
 jouerIA(G):- jouerIA(0,G).
 
-jouerCoupJoueur(G):- write('Joueur A, entrez un numéro de colonne : '),
-				read(N), enregistrerCoupJoueur(N,G, a, X, G),
+jouerCoupJoueur(G):- write('Joueur x, entrez un numéro de colonne : '),
+				read(N), enregistrerCoupJoueur(N,G, x, X, G),
 				afficherGrille(X),
 				write('\n'),
 				jouerIA(X).
@@ -217,16 +255,16 @@ tracerArbre(G, J, P, A):- P > 0,
 						  tracerBranche(G, J, P1, A, 7).
 
 tracerBranche(G, J, P, A, 1).						  
-tracerBranche(G, a, P, A, N):- N > 0,
+tracerBranche(G, x, P, A, N):- N > 0,
 							   N1 is N-1,
-							   enregistrerCoupArbre(N, G, a, X), 
-							   tracerArbre(X, b, P, A),
-							   tracerBranche(G, a, P, A, N1).			
-tracerBranche(G, b, P, A, N):- N > 0,
+							   enregistrerCoupArbre(N, G, x, X), 
+							   tracerArbre(X, o, P, A),
+							   tracerBranche(G, x, P, A, N1).			
+tracerBranche(G, o, P, A, N):- N > 0,
 							   N1 is N-1,
-							   enregistrerCoupArbre(N, G, b, X), 
-							   tracerArbre(X, a, P, A),
-							   tracerBranche(G, b, P, A, N1).
+							   enregistrerCoupArbre(N, G, o, X), 
+							   tracerArbre(X, x, P, A),
+							   tracerBranche(G, o, P, A, N1).
 afficherGrille(_,0).							   
 afficherGrille(G, N):-	 N > 0,
 						N1 is N-1,
